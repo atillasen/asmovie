04b37327e6efeec817b306f593874d65f0434f31
Atilla Sen <home@atilla-sen.com>
2020-01-12 21:32:23 +0100

    3

--- src/main/java/asmovieDao/User.java
+++ src/main/java/asmovieDao/User.java
@@ -15,6 +15,20 @@ public class User implements Serializable {
 
     private String userName;
 
+    private String password;
+
+
+    public User() {
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public Long getId() {
         return id;
     }
